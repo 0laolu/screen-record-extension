0143ca510e8af0d5272b1fe64598f532ad3ffb1b
0laolu <abdullateefolaolu@gmail.com>
2023-10-02 13:53:20 +0100

    feat: finish building desktop view for how-it-works section

--- src/components/landingPage/HowItWorks.jsx
+++ src/components/landingPage/HowItWorks.jsx
@@ -8,13 +8,13 @@ export default function HowItWorks() {
     return (
         <section className="how-it-works-section">
             <div className="how-it-works-content-wrapper">
-                <div className="section-header">How It Works</div>
+                <h3 className="section-header">How It Works</h3>
 
                 <div className="how-it-works-card-container">
                     <div className="how-it-works-card">
                         <div className="how-it-works-card-content">
                             <img src={One} alt="" />
-                            <div className="card-content-header">Record Screen</div>
+                            <h3 className="card-content-header">Record Screen</h3>
                             <p className="description">
                                 Click the "Start Recording" button in our extension.  choose which part of your screen to capture and who you want to send it to.
                             </p>
@@ -28,7 +28,7 @@ export default function HowItWorks() {
                     <div className="how-it-works-card">
                         <div className="how-it-works-card-content">
                             <img src={Two} alt="" />
-                            <div className="card-content-header">Share Your Recording</div>
+                            <h3 className="card-content-header">Share Your Recording</h3>
                             <p className="description">
                             We generate a shareable link for your video. Simply send it to your audience via email or copy the link to send via any platform.
                             </p>
@@ -42,7 +42,7 @@ export default function HowItWorks() {
                     <div className="how-it-works-card">
                         <div className="how-it-works-card-content">
                             <img src={Three} alt="" />
-                            <div className="card-content-header">Learn Effortlessly</div>
+                            <h3 className="card-content-header">Learn Effortlessly</h3>
                             <p className="description">
                             Recipients can access your video effortlessly through the provided link, with our user-friendly interface suitable for everyone.
                             </p>
